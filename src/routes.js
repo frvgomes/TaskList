eeee1612acced8c3d551626ce4385965861e8849
Francisco Rosemberg <frvgomes@gmail.com>
2023-08-22 08:35:01 -0300

    Configuration fisrt connection app to database

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => res.json({ ok: true }));
+routes.get('/teste', async (req, res) => {
+    const user = await User.create({
+        name: 'Admin',
+        email: 'admin@teste.com.br',
+        password_hash: '123456',
+    });
+
+    return res.json(user);
+});
 
 export default routes;
