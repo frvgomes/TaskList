b2479e5a1ba8fb8755577e4061e049fb50c9572d
Francisco Rosemberg <frvgomes@gmail.com>
2023-08-23 08:19:56 -0300

    Create migration and model to task entity

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import { Sequelize } from 'sequelize';
 import databaseconfig from '../config/database';
 
 import User from '../app/models/Users';
+import Task from '../app/models/Task';
 
-const models = [User];
+const models = [User, Task];
 
 class Database {
     constructor() {
@@ -13,7 +14,12 @@ class Database {
     init() {
         this.connection = new Sequelize(databaseconfig);
 
-        models.map((model) => model.init(this.connection));
+        models
+            .map((model) => model.init(this.connection))
+            .map(
+                (model) =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
