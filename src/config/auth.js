88736777893f618832f9b299fb7a920787912982
Francisco Rosemberg <frvgomes@gmail.com>
2023-08-23 06:55:37 -0300

    Add validation user login with jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '036d197cb995ca7f50238aaf695308fb',
+    expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
+routes.post('/login', SessionController.store);
 routes.post('/users', UserController.store);
+
 export default routes;
